a54670d6b1e4f4ffd4bd59b64268b8a37bc67335
pugmarx <naqi.rizvi@gmail.com>
2020-03-26 15:28:28 +0000

    sample openfaas python with node constraint

---
+++ python-hello-sw/handler.py
@@ -0,0 +1,7 @@
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+    print("Hello from the other side!! You said: " + req)
+    #return req
